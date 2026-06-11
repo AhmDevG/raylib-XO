dfd13d3d553135e90dd1923bd36c79b9cfe3f7e5
ma9737149 <ahmedashraf3695@gmail.com>
2026-06-11 09:25:50 +0300

    draw board and content

--- main.cpp
+++ main.cpp
@@ -12,19 +12,24 @@ using namespace std;
 const int WIDTH = 500;
 const int HEIGHT = 500;
 const int BUTTON_SIZE = 110;
-const int OFFSET = 10 + BUTTON_SIZE;
 
 
 void DrawBoard(vector<vector<string>> &board){
     int row_size = board.size();
     int col_size = board[0].size();
-    for (int i = 0 ; i < row_size ; i++){
-        for (int j = 0; j < col_size; j++) {
-            int x_position = 95 + (OFFSET * j) ;
-            int y_position = 95 + (OFFSET * i) ;
+    for (int i = 0 ; i <= row_size ; i++){
+        for (int j = 0; j <= col_size; j++) {
+            int position = 90 + (BUTTON_SIZE * i);
+            int board_size =  90 + (BUTTON_SIZE * 3);
 
-            // DrawRectangle(x_position , y_position , BUTTON_SIZE, BUTTON_SIZE, GRAY);
-            // DrawText(board[i][j].c_str() , 28 + x_position , 10 + y_position , 80  , WHITE);
+            if (i < row_size && j < col_size && board[i][j] != ""){
+                int x_position = 90 + (BUTTON_SIZE * j);
+                int y_position = 90 + (BUTTON_SIZE * i);                                                 
+                DrawText(board[i][j].c_str(), x_position + 30, y_position + 20, 80 , WHITE);             
+            }
+
+            DrawLine(90 , position, board_size, position, WHITE) ;
+            DrawLine(position, 90 , position, board_size, WHITE);
         }
     }
 }
@@ -41,10 +46,16 @@ int main(){
     // board[0][1] = "X";
     // board[0][2] = "X";
     //
-    // board[0][0] = "O";
-    // board[1][0] = "O";
+    //
+    // board[1][0] = "X";
+    // board[1][1] = "X";
+    // board[1][2] = "X";
+    //
+    //
     // board[2][0] = "O";
-
+    // board[2][1] = "O";
+    // board[2][2] = "O";
+    
     random_device rd;
     mt19937 gen(rd());
 
@@ -60,22 +71,9 @@ int main(){
         BeginDrawing();
         ClearBackground(BLACK);
         DrawText((players[randomIndex] + " Turn").c_str(), 180, 15, 40, WHITE);
-        //DrawBoard(board);
-        // 95 + (OFFSET * j) ;
-        // 95 + (OFFSET * i) ;
-        // 90 + 2
-        
-        // x - AXIS 
-        DrawLine(90,  90,  420,  90 ,  WHITE);
-        DrawLine(90,  BUTTON_SIZE + 90     ,  420 ,  90 + BUTTON_SIZE ,  WHITE); 
-        DrawLine(90,  BUTTON_SIZE * 2 + 90 ,  420 ,  90 + BUTTON_SIZE*2 ,  WHITE);
-        DrawLine(90,  BUTTON_SIZE * 3 + 90 ,  420,  90 + BUTTON_SIZE*3 ,  WHITE);
 
-        // Y - AXIS
-        DrawLine(90,  90,  90,  90 + BUTTON_SIZE*3 ,  WHITE);
-        DrawLine(90 + BUTTON_SIZE,  90,  90 + BUTTON_SIZE,  90 + BUTTON_SIZE*3 ,  WHITE);
-        DrawLine(90 + BUTTON_SIZE*2,  90,  90 + BUTTON_SIZE*2,  90 + BUTTON_SIZE*3 ,  WHITE);
-        DrawLine(90 + BUTTON_SIZE*3,  90,  90 + BUTTON_SIZE*3 ,  90 + BUTTON_SIZE*3 ,  WHITE);
+        DrawBoard(board) ;
+        
 
         EndDrawing();
     }
