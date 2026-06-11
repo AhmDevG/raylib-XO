bd20b522437c6855f557d1dc6ac6b9884292761f
ma9737149 <ahmedashraf3695@gmail.com>
2026-06-11 10:48:36 +0300

    added handle click function to handle x o moves and turns

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "include/raylib.h"
 #include <random>
+#include <utility>
 #include <vector>
 #include <string>
+#include <map>
 
 using namespace std;
 
@@ -12,9 +14,51 @@ using namespace std;
 const int WIDTH = 500;
 const int HEIGHT = 500;
 const int BUTTON_SIZE = 110;
+Rectangle top_left     = { 90,  90, 110, 110};
+Rectangle top_middle   = {200,  90, 110, 110};
+Rectangle top_right    = {310,  90, 110, 110};
+
+Rectangle middle_left  = { 90, 200, 110, 110};
+Rectangle center       = {200, 200, 110, 110};
+Rectangle middle_right = {310, 200, 110, 110};
+
+Rectangle bottom_left  = { 90, 310, 110, 110};
+Rectangle bottom_mid   = {200, 310, 110, 110};
+Rectangle bottom_right = {310, 310, 110, 110};
+
+vector<string> players {"X" , "O"};
+vector<vector<string>> board(3 , vector<string>(3 , ""));
+int row_size = 3;
+int col_size = 3;
 
 
-void DrawBoard(vector<vector<string>> &board){
+random_device rd;
+mt19937 gen(rd());
+
+uniform_int_distribution<size_t> dist(0 , players.size() - 1);
+
+size_t turn = dist(gen);
+
+
+struct Positions {
+    Vector2 TOPLEFT = {0 , 0};
+    Vector2 TOPMID = {0 , 1};
+    Vector2 TOPRIGHT = {0 , 2};
+
+    Vector2 MIDLEFT = {1 , 0};
+    Vector2 CENTER = {1 , 1};
+    Vector2 MIDRIGHT = {1 , 2};
+
+
+    Vector2 BOTTOMLEFT = {2 , 0};
+    Vector2 BOTTOMMID = {2 , 1};
+    Vector2 BOTTOMRIGHT = {2 , 2};
+};
+
+
+
+
+void DrawBoard(){
     int row_size = board.size();
     int col_size = board[0].size();
     for (int i = 0 ; i <= row_size ; i++){
@@ -34,12 +78,127 @@ void DrawBoard(vector<vector<string>> &board){
     }
 }
 
+Positions positions ;
+
+void handleClick(Vector2 &mousePoint){
+        if(CheckCollisionPointRec(mousePoint, top_left)){
+           if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+               int i = positions.TOPLEFT.x;
+               int j = positions.TOPLEFT.y;
+
+               if (board[i][j] != "") return;
+               board[i][j] = players[turn];
+
+                if (turn == 1) turn = 0;
+                else turn = 1;
+           }
+        }
+        if(CheckCollisionPointRec(mousePoint, top_middle)){
+           if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+               int i = positions.TOPMID.x;
+               int j = positions.TOPMID.y;
+
+               if (board[i][j] != "") return;
+               board[i][j] = players[turn];
+
+                if (turn == 1) turn = 0;
+                else turn = 1;
+           }
+        }
+        if(CheckCollisionPointRec(mousePoint, top_right)){
+            if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+               int i = positions.TOPRIGHT.x;
+               int j = positions.TOPRIGHT.y;
+
+               if (board[i][j] != "") return;
+               board[i][j] = players[turn];
+
+                if (turn == 1) turn = 0;
+                else turn = 1;
+            }
+        }
+
+        if(CheckCollisionPointRec(mousePoint, middle_left)){
+            if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+               int i = positions.MIDLEFT.x;
+               int j = positions.MIDLEFT.y;
+
+               if (board[i][j] != "") return;
+               board[i][j] = players[turn];
+
+                if (turn == 1) turn = 0;
+                else turn = 1;
+            }
+        }
+        if(CheckCollisionPointRec(mousePoint, center)){
+            if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+               int i = positions.CENTER.x;
+               int j = positions.CENTER.y;
+
+               if (board[i][j] != "") return;
+               board[i][j] = players[turn];
+
+                if (turn == 1) turn = 0;
+                else turn = 1;
+            }
+        }
+        if(CheckCollisionPointRec(mousePoint, middle_right)){
+            if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+               int i = positions.MIDRIGHT.x;
+               int j = positions.MIDRIGHT.y;
+
+               if (board[i][j] != "") return;
+               board[i][j] = players[turn];
+
+                if (turn == 1) turn = 0;
+                else turn = 1;
+            }
+        }
+
+
+        if(CheckCollisionPointRec(mousePoint, bottom_left)){
+            if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+               int i = positions.BOTTOMLEFT.x;
+               int j = positions.BOTTOMLEFT.y;
+
+               if (board[i][j] != "") return;
+               board[i][j] = players[turn];
+
+                if (turn == 1) turn = 0;
+                else turn = 1;
+            }
+        }
+        if(CheckCollisionPointRec(mousePoint, bottom_mid)){
+            if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+               int i = positions.BOTTOMMID.x;
+               int j = positions.BOTTOMMID.y;
+
+               if (board[i][j] != "") return;
+               board[i][j] = players[turn];
+
+                if (turn == 1) turn = 0;
+                else turn = 1;
+            }
+        }
+        if(CheckCollisionPointRec(mousePoint, bottom_right)){
+            if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+               int i = positions.BOTTOMRIGHT.x;
+               int j = positions.BOTTOMRIGHT.y;
+
+               if (board[i][j] != "") return;
+               board[i][j] = players[turn];
+
+                if (turn == 1) turn = 0;
+                else turn = 1;
+            }
+        }
+
+}
+
 
 int main(){
     InitWindow(WIDTH , HEIGHT, "XO game");
 
-    vector<string> players {"X" , "O"};
-    vector<vector<string>> board(3 , vector<string>(3 , ""));
 
     // for testing
     // board[0][0] = "X";
@@ -56,24 +215,25 @@ int main(){
     // board[2][1] = "O";
     // board[2][2] = "O";
     
-    random_device rd;
-    mt19937 gen(rd());
-
-    uniform_int_distribution<size_t> dist(0 , players.size() - 1);
-
-    size_t randomIndex = dist(gen);
 
 
     SetTargetFPS(60);
 
 
+
+
     while(!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawText((players[randomIndex] + " Turn").c_str(), 180, 15, 40, WHITE);
+        DrawText((players[turn] + " Turn").c_str(), 180, 15, 40, WHITE);
 
-        DrawBoard(board) ;
-        
+        DrawBoard();
+
+        Vector2 mousePoint = GetMousePosition();
+        handleClick(mousePoint);
+
+
+
 
         EndDrawing();
     }
